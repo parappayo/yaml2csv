f6e04c00fe5b903d5e3fe6da92469ad4f712bc55
Jason Estey <esteyj@yahoo.com>
2018-10-02 16:33:05 -0700

    can write some csv, lists are not properly handled

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # yaml2csv
-Convert input stream of yaml docs into output stream of csv rows
+
+Convert input stream of yaml docs into output stream of csv rows.
+
+Built for [.NET Core](https://dotnet.github.io/) using [YamlDotNet](https://github.com/aaubry/YamlDotNet) and [CsvHelper](https://joshclose.github.io/CsvHelper/).
+
+# Links
+
+* [ConvertYamlToJson Example](https://github.com/aaubry/YamlDotNet/wiki/Samples.ConvertYamlToJson)
--- yaml2csv/Program.cs
+++ yaml2csv/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CsvHelper;
 
 namespace yaml2csv
 {
@@ -14,12 +16,38 @@ namespace yaml2csv
             "  - three\n" +
             "...";
 
+        static void WriteCsvHeaders(CsvSerializer csv, Dictionary<string, Object> data)
+        {
+            string[] keys = new string[data.Keys.Count];
+            data.Keys.CopyTo(keys, 0);
+
+            csv.Write(keys);
+            csv.WriteLine();
+        }
+
+        static void WriteCsvValues(CsvSerializer csv, Dictionary<string, Object> data)
+        {
+            object[] valueObjs = new object[data.Values.Count];
+            data.Values.CopyTo(valueObjs, 0);
+
+            string[] values = new string[valueObjs.Length];
+            for (int i = 0; i < valueObjs.Length; i++)
+            {
+                values[i] = valueObjs[i].ToString();
+            }
+
+            csv.Write(values);
+            csv.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            var deserializer = new YamlDotNet.Serialization.Deserializer();
-            Object data = deserializer.Deserialize<Object>(TestYamlDoc);
+            var yamlDeserializer = new YamlDotNet.Serialization.Deserializer();
+            var data = yamlDeserializer.Deserialize<Dictionary<string, Object>>(TestYamlDoc);
 
-            Console.Write(data);
+            var csv = new CsvSerializer(Console.Out);
+            WriteCsvHeaders(csv, data);
+            WriteCsvValues(csv, data);
         }
     }
 }
