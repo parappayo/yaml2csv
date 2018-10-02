8e425becb39f8a7324547c9248a558813a4f08f6
Jason Estey <esteyj@yahoo.com>
2018-10-02 15:39:14 -0700

    create empty .NET Core project

---
+++ yaml2csv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace yaml2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
