442705be0a413678f9f0e84fc8e39a7e38f2f0f4
Jason Estey <esteyj@yahoo.com>
2018-10-02 15:50:10 -0700

    deserialize some test data

--- yaml2csv/Program.cs
+++ yaml2csv/Program.cs
@@ -4,9 +4,22 @@ namespace yaml2csv
 {
     class Program
     {
+        public static string TestYamlDoc =
+            "---\n" +
+            "id: 123\n" +
+            "name: \"Test Data Set\"\n" +
+            "myList:\n" +
+            "  - one\n" +
+            "  - two\n" +
+            "  - three\n" +
+            "...";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            Object data = deserializer.Deserialize<Object>(TestYamlDoc);
+
+            Console.Write(data);
         }
     }
 }
